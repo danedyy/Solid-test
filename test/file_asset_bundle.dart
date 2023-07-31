86dba159226289dd1cdbf784d6adfdd6da40ce88
Edim Daniel <edim1925.de@gmail.com>
2023-07-31 23:42:42 +0100

    removed unused files

--- test/file_asset_bundle.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/services.dart';
-import 'package:golden_toolkit/src/test_asset_bundle.dart';
-
-class FileAssetBundle extends TestAssetBundle {
-  @override
-  Future<ByteData> load(String key) {
-    return rootBundle.load(key);
-  }
-}
